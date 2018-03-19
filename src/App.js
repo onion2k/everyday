a48a1da36a41ecf06a19effdde9c7623f1611d4a
Chris Neale <chris@usablehq.com>
2018-03-19 20:40:52 +0000

    Grid of days

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      days:[]
+    }
+    for (let x=0;x<366;x++) {
+      this.state.days.push(Math.floor(Math.random()*12));
+    }
+  }
   render() {
+    const days = this.state.days.map((day)=>{
+      let productivity = 0;
+      let classes = ['day'];
+      if (day > 0) {
+        productivity = 1 + Math.floor(day / 4);
+        switch (productivity) {
+          case 0: classes.push(`prod_low`); break;
+          case 1: classes.push(`prod_mid`); break;
+          case 2: classes.push(`prod_high`); break;
+          case 3: classes.push(`prod_max`); break;
+        }
+      }
+      return <div className={classes.join(' ')}></div>
+    });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main id="graph" className="graph">
+          {days}
+        </main>
       </div>
     );
   }
