35e77164b9bbe1e98d100d37ba94c8c55b759625
Chris Neale <chris@usablehq.com>
2018-03-23 06:55:23 +0000

    Working layout

--- src/App.js
+++ src/App.js
@@ -6,52 +6,78 @@ import './App.css';
 class App extends Component {
   constructor(props){
     super(props);
+    this.m = Object.keys(months).map((month, i) => { return months[month].days; });
     this.state = {
       days:[],
-      colors: palx('#88ff00'),
-      color: "violet",
-      m: Object.keys(months).map((month, i) => { return months[month].days; })
+      colors: palx('#f00'),
+      color: "violet"
     }
-    for (let x=0;x<365;x++) {
-      this.state.days.push(Math.floor(Math.random()*12));
+    for (let x=0;x<366;x++) {
+      this.state.days.push(12);
     }
   }
-  render() {
-    const days = this.state.days.map((day, i)=>{
+  days(n, color){
+    let days = [];
+    for(let x=0; x < n; x++) {
       let productivity = 0;
       let classes = ['day'];
+      let day = this.state.days[x];
       if (day > 0) {
         productivity = 1 + Math.floor(day / 5);
       }
-      return <div className={'day'} style={{ backgroundColor: this.state.colors[this.state.color][productivity*2] }}></div>
-    });
+      days.push(<div className={'day'} style={{ backgroundColor: this.state.colors[color][productivity*2] }}></div>);
+    }
+    return days;
+  }
+  render() {
     return (
       <div className="App">
         <h1>_____________ every day</h1>
         <main id="graph" className="graph">
 
+          <div className="sunday"></div>
+          <div className="weekday">Mon</div>
+          <div className="weekday">Wed</div>
+          <div className="weekday">Fri</div>
+          
           <div className="month start"></div>
           <div className="month jan">Jan</div>
-          <div className="month feb">Feb</div>
-          <div className="month mar">Mar</div>
-          <div className="month apr">Apr</div>
-          <div className="month may">May</div>
-          <div className="month june">June</div>
-          <div className="month july">July</div>
-          <div className="month aug">Aug</div>
-          <div className="month sept">Sept</div>
-          <div className="month oct">Oct</div>
-          <div className="month nov">Nov</div>
-          <div className="month dec">Dec</div>
-
-          <div className="sunday"></div>
-          <div className="weekday">Monday</div>
-          <div className="weekday">Wednesday</div>
-          <div className="weekday">Friday</div>
-
           <div className="empty"></div>
+          {this.days(this.m[0],"red")}
 
-          {days}
+          <div className="month feb">Feb</div>
+          {this.days(this.m[1],"orange")}
+
+          <div className="month mar">Mar</div>
+          {this.days(this.m[2],"yellow")}
+
+          <div className="month apr">Apr</div>
+          {this.days(this.m[3],"lime")}
+
+          <div className="month may">May</div>
+          {this.days(this.m[4],"green")}
+
+          <div className="month june">June</div>
+          {this.days(this.m[5],"teal")}
+
+          <div className="month july">July</div>
+          {this.days(this.m[6],"cyan")}
+
+          <div className="month aug">Aug</div>
+          {this.days(this.m[7],"blue")}
+
+          <div className="month sept">Sept</div>
+          {this.days(this.m[8],"indigo")}
+
+          <div className="month oct">Oct</div>
+          {this.days(this.m[9],"violet")}
+
+          <div className="month nov">Nov</div>
+          {this.days(this.m[10],"pink")}
+
+          <div className="month dec">Dec</div>
+          {this.days(this.m[11],"gray")}
+
         </main>
       </div>
     );
