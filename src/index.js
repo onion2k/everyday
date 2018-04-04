e489ce39d027b648a819d4d38aef570449726abd
Chris Neale <chris@komododigital.co.uk>
2018-04-04 12:50:05 +0100

    Firebase queries

--- src/index.js
+++ src/index.js
@@ -2,15 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+// gitignored so the details don't get in to the repo. Create a local version
 import firebase from './firebase';
+
 // import registerServiceWorker from './registerServiceWorker';
 
-// const email = "";
-// const password = "";
+// const email = "chris@usablehq.com";
+// const password = "password";
 
-// let user = firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-// 	console.log(error);
-// });
+// firebase.auth().signInWithEmailAndPassword(email, password)
+//   .then((d) => {
+//     let user = d;
+//     let firestore = firebase.firestore();
+
+//     // firestore.collection('charts').add({ activity: 'write' }); // add with auto id
+//     // firestore.collection('charts').doc('sing').set({ activity: 'sing' }); // upsert with defined id
+
+//   })
+//   .catch(function(error) {
+//     console.log(error);
+//   });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
