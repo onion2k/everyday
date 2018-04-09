3c1027c91b97ad6bb8f6a73da34705af6da8ec8b
Chris Neale <chris@komododigital.co.uk>
2018-04-09 12:35:27 +0100

    Login form hooked up to React

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -3,20 +3,46 @@ import { Link } from 'react-router-dom'
 import './Login.css';
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {email: '', password: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.email+' '+this.state.password);
+  }
 
   render(){
 
     let email = {
       type: "email",
       name: "email",
-      value: "",
-      placeholder: "chris@everyday.com"
+      autoComplete: "email",
+      value: this.state.email,
+      placeholder: "chris@everyday.com",
+      onChange: this.handleChange
     }
     
     let password = {
       type: "password",
       name: "password",
-      value: ""
+      autoComplete: "current-password",
+      value: this.state.password,
+      onChange: this.handleChange
     }
 
     return (
@@ -25,7 +51,7 @@ export default class Login extends Component {
             Sign in to your Everyday account
           </div>
 
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <div>
               <label>Email address</label>
               <input {...email} />
@@ -33,6 +59,9 @@ export default class Login extends Component {
             <div>
               <label>Password</label>
               <input {...password} />
+            </div>
+            <div>
+              <input type="submit" value="Create" />
             </div>
           </form>
 
