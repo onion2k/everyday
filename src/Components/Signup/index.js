31851753d2b1ad98ed72cf1f551299175f4813ed
Chris Neale <chris@komododigital.co.uk>
2018-04-11 08:45:08 +0100

    Attempt at accessible combo box

--- src/Components/Signup/index.js
+++ src/Components/Signup/index.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import firebase from '../../firebase';
+import Combobo from 'combobo';
 import './Signup.css';
 
 export default class Signup extends Component {
   constructor(props) {
     super(props);
-    this.state = {email: 'chris@usablehq.com', password: 'asd'};
+    this.state = {email: 'chris@usablehq.com', password: 'asd', chart: 'Code' };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount(){
+
+    const chart = new Combobo({input: '.combobox',
+      list: '.listbox',
+      options: '.option', 
+      openClass: 'open',
+      activeClass: 'active',
+      selectedClass: 'selected',
+      useLiveRegion: true,
+      noResultsText: null,
+      selectionValue: (selecteds) => selecteds.map((s) => s.innerText.trim()).join(' - '),
+      optionValue: 'underline', // wrap the matched portion of the option (if applicable) in a span with class "underline"
+      filter: 'contains' // 'starts-with', 'equals', or funk
+    });
+
+    chart.on('change', (e) => {
+      this.setState({
+        'chart': chart.currentOption.innerText
+      });
+    });
   }
 
   handleChange(event) {
@@ -24,38 +47,39 @@ export default class Signup extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    if (firebase.auth) {
 
-      firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-        .then((d) => {
-          let user = d;
-          let firestore = firebase.firestore();
+    // if (firebase.auth) {
 
-          console.log(user)
+    //   firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+    //     .then((d) => {
+    //       let user = d;
+    //       let firestore = firebase.firestore();
 
-          firestore.collection('activity').add({
-            activity: 'write',
-            note: '',
-            timestamp: Date.now(),
-            uid: user.uid
-          });
+    //       console.log(user)
 
-          // firestore.collection('charts').doc('sing').set({ activity: 'sing' }); // upsert with defined id
+    //       firestore.collection('activity').add({
+    //         activity: 'write',
+    //         note: '',
+    //         timestamp: Date.now(),
+    //         uid: user.uid
+    //       });
 
-          firestore.collection('charts').where('enabled', '==', true).get().then((querySnapshot)=>{
-            querySnapshot.forEach((doc)=>{
-              // doc.data() is never undefined for query doc snapshots
-              console.log(doc.id, " => ", doc.data());
-              this.props.history.push('/u/onion');
-            });
-          });
+    //       // firestore.collection('charts').doc('sing').set({ activity: 'sing' }); // upsert with defined id
+
+    //       firestore.collection('charts').where('enabled', '==', true).get().then((querySnapshot)=>{
+    //         querySnapshot.forEach((doc)=>{
+    //           // doc.data() is never undefined for query doc snapshots
+    //           console.log(doc.id, " => ", doc.data());
+    //           this.props.history.push('/u/onion');
+    //         });
+    //       });
 
 
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-    }
+    //     })
+    //     .catch(function(error) {
+    //       console.log(error);
+    //     });
+    // }
 
   }
 
@@ -98,13 +122,23 @@ export default class Signup extends Component {
               </div>
               <div>
                 <label>Chart</label>
-                <select>
-                  <option>Read</option>
-                  <option>Code</option>
-                  <option>Write</option>
-                  <option>Sing</option>
-                  <option>Journal</option>
-                </select>
+                <div className="combo-wrap">
+                  <input type="text" name="chart" className="combobox" id="combobox-single" onChange={this.handleChange} value={this.state.chart} />
+                  <i aria-hidden="true" className="fa trigger fa-caret-down" data-trigger="single"></i>
+                  <div className="listbox">
+                    <div className="option">Ween</div>
+                    <div className="option">Frank Zappa</div>
+                    <div className="option">Snarky Puppy</div>
+                    <div className="option">Umphrey's McGee</div>
+                    <div className="option">Keller Williams</div>
+                    <div className="option">Greensky Bluegrass</div>
+                    <div className="option">Leftover Salmon</div>
+                    <div className="option">Code</div>
+                    <div className="option">Family Groove Company</div>
+                    <div className="option">Mac Demarco</div>
+                    <div className="option">Lettuce</div>
+                  </div>
+                </div>
               </div>
 
               <div>
